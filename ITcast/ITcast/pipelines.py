4dd00175a1b164c5ffcd22eb54aa0b54214d7ced
jurongbing <719203729@qq.com>
2020-03-27 21:37:07 +0800

    add scrapy module

---
+++ ITcast/ITcast/pipelines.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+import json
+class ItcastPipeline(object):
+    def __init__(self):
+        # python3保存文件 必须需要'wb'  保存为json格式
+        self.f = open("itcast_pipeline.json", 'wb')
+
+    def process_item(self, item, spider):
+        # 读取item中的数据 并换行处理
+        content = json.dumps(dict(item), ensure_ascii=False) + ',\n'
+        self.f.write(content.encode('utf=8'))
+        return item
+
+    def close_spider(self, spider):
+        # 关闭文件
+        self.f.close()
