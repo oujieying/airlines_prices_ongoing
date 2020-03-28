62c2bd88609f7e65c89cbf537a4588dcd5440706
jurongbing <719203729@qq.com>
2020-03-29 00:51:06 +0800

    select teachers byname [web level]

--- airlines-web/app.py
+++ airlines-web/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-
+from SelectTeacher import TeacherSelect
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+@app.route('/showTearchersByName')
+def showTearchersByName(name):
+    return TeacherSelect.selectByName(name)
 
 
 if __name__ == '__main__':
---
+++ airlines-web/service/SelectTeacher.py
@@ -0,0 +1,9 @@
+from MysqlUtils import DBConn
+class TeacherSelect(object):
+    def __init__(self):
+        pass
+        # self.conn = DBConn("127.0.0.1","root","root","teachers_for_test","")
+    def selectByName(self,name):
+        db = DBConn("127.0.0.1", "root", "root", "airlines", "")
+        sql  = "select id,name,title,info from teachers_for_test where name ="+name;
+        return db.searchByCondition(sql)
